9eccadab56a15ad280cbbb44e282dfb1f7930410
meetFeeltech <meet.feeltechsolution@outlook.com>
2025-07-31 17:13:20 +0530

    31-07-2025 : Port Folio changes

--- lib/Utils/global_function.dart
+++ lib/Utils/global_function.dart
@@ -1,24 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchMap() async {
-  const double lat = 23.095826377558456;
-  const double lng = 72.66432418841963;
-  final Uri googleMapUrl = Uri.parse("https://www.google.com/maps/search/?api=1&query=$lat,$lng");
+  final Uri googleMapUrl = Uri.parse(
+    'https://www.google.com/maps/search/?api=1&query=23.095826377558456,72.66432418841963',
+  );
 
-  if (await canLaunchUrl(googleMapUrl)) {
-    await launchUrl(googleMapUrl);
-  } else {
-    throw 'Could not launch map';
+  final bool launched = await launchUrl(
+    googleMapUrl,
+    mode: LaunchMode.externalApplication,
+  );
+
+  if (!launched) {
+    debugPrint('Could not launch map');
   }
 }
 
 Future<void> launchPhoneDialer() async {
   final Uri phoneUri = Uri(scheme: 'tel', path: '+919924859611');
 
-  if (await canLaunchUrl(phoneUri)) {
-    await launchUrl(phoneUri);
-  } else {
-    throw 'Could not launch phone dialer';
+  final bool launched = await launchUrl(
+    phoneUri,
+    mode: LaunchMode.externalApplication,
+  );
+
+  if (!launched) {
+    debugPrint('Could not launch phone dialer');
   }
 }
 
@@ -26,12 +33,15 @@ Future<void> launchEmail() async {
   final Uri emailUri = Uri(
     scheme: 'mailto',
     path: 'mevadameet8301@gmail.com',
-    query: 'subject=Contact&body=Hello Meet,', // optional
+    query: Uri.encodeFull('subject=Contact&body=Hello Meet,'),
   );
 
-  if (await canLaunchUrl(emailUri)) {
-    await launchUrl(emailUri);
-  } else {
-    throw 'Could not launch email';
+  final bool launched = await launchUrl(
+    emailUri,
+    mode: LaunchMode.externalApplication,
+  );
+
+  if (!launched) {
+    debugPrint('Could not launch email');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'Screens/portfolio.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
